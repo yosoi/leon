6a60b7193d376fe02aa7d58bd5bc053015a2435e
Yosoi <yosoi@protonmail.com>
2020-06-08 15:55:44 -0400

    install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button variant="primary">Primary</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
