b4e104cf6a9ceae75a7e136dd091865a29c6fd21
Yosoi <yosoi@protonmail.com>
2020-06-12 14:23:32 -0400

    icons as components. correct icon for layer type.

--- src/components/Layer.js
+++ src/components/Layer.js
@@ -4,14 +4,19 @@ import Button from 'react-bootstrap/Button'
 import ButtonGroup from 'react-bootstrap/ButtonGroup'
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
 import Card from 'react-bootstrap/Card'
+import CloseIcon from './icons/closeIcon.js'
 import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
+import DownIcon from './icons/downIcon.js'
 import Dropdown from 'react-bootstrap/Dropdown'
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import FormControl from 'react-bootstrap/FormControl'
 import InputGroup from 'react-bootstrap/InputGroup'
 import LayerType from '../enums/layerType.js'
+import RightIcon from './icons/rightIcon.js'
 import Row from 'react-bootstrap/Row'
+import UpIcon from './icons/upIcon.js'
+import VolumeIcon from './icons/volumeIcon.js'
 
 function Layer(props) {
   const layer = props.layer;
@@ -25,25 +30,18 @@ function Layer(props) {
             variant="light"
             className="mr-2"
           >
-            <svg className="bi bi-caret-right-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-              <path d="M12.14 8.753l-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
-            </svg>
+            <RightIcon />
           </Accordion.Toggle>
           <InputGroup className="mr-2">
             <InputGroup.Prepend>
-              <InputGroup.Text id="btnGroupAddon">
-                <svg className="bi bi-music-note-beamed" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M6 13c0 1.105-1.12 2-2.5 2S1 14.105 1 13c0-1.104 1.12-2 2.5-2s2.5.896 2.5 2zm9-2c0 1.105-1.12 2-2.5 2s-2.5-.895-2.5-2 1.12-2 2.5-2 2.5.895 2.5 2z"/>
-                  <path fillRule="evenodd" d="M14 11V2h1v9h-1zM6 3v10H5V3h1z"/>
-                  <path d="M5 2.905a1 1 0 0 1 .9-.995l8-.8a1 1 0 0 1 1.1.995V3L5 4V2.905z"/>
-                </svg>
+              <InputGroup.Text>
+                {LayerType.properties[layer.type].icon}
               </InputGroup.Text>
             </InputGroup.Prepend>
             <FormControl
               type="text"
               placeholder={LayerType.properties[layer.type].name}
               aria-label="Layer name field"
-              aria-describedby="btnGroupAddon"
             />
           </InputGroup>
           <ButtonGroup className="mr-2">
@@ -52,21 +50,14 @@ function Layer(props) {
           </ButtonGroup>
           <ButtonGroup className="mr-2">
             <Button variant="outline-secondary">
-              <svg className="bi bi-caret-up-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path d="M7.247 4.86l-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/>
-              </svg>
+              <UpIcon />
             </Button>
             <Button variant="outline-secondary">
-              <svg className="bi bi-caret-down-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path d="M7.247 11.14L2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
-              </svg>
+              <DownIcon />
             </Button>
           </ButtonGroup>
           <Button variant="light">
-            <svg className="bi bi-x" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-              <path fillRule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"/>
-              <path fillRule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"/>
-            </svg>
+            <CloseIcon />
           </Button>
         </ButtonToolbar>
       </Card.Header>
--- src/enums/layerType.js
+++ src/enums/layerType.js
@@ -1,12 +1,17 @@
+import VolumeIcon from '../components/icons/volumeIcon.js'
+import PitchIcon from '../components/icons/pitchIcon.js'
+
 export default Object.freeze({
   VOLUME: 1,
   PITCH: 2,
   properties: {
     1: {
+      icon: VolumeIcon(),
       name: "Volume",
       value: 1
     },
     2: {
+      icon: PitchIcon(),
       name: "Pitch",
       value: 2
     }
