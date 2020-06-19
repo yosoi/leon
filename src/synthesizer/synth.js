6109e64a3decffdf837bba50487b2a6b1576301e
Yosoi <yosoi@protonmail.com>
2020-06-19 16:33:36 -0400

    null propagation in synth.configure()

--- src/synthesizer/synth.js
+++ src/synthesizer/synth.js
@@ -4,14 +4,14 @@ import * as Tone from "tone";
 function main() {
   this.synth = new Tone.Synth().toMaster();
   this.configure = function(settings) {
-    const pitch = settings[LayerType.PITCH].value;
-    const volume = settings[LayerType.VOLUME].value;
-    console.log(pitch, volume);
-    this.synth.setNote(pitch);
-    this.synth.volume.value = volume;
+    const pitch = settings[LayerType.PITCH]?.value;
+    const volume = settings[LayerType.VOLUME]?.value;
+    if (pitch !== undefined) this.synth.setNote(pitch);
+    if (volume !== undefined) this.synth.volume.value = volume;
   };
   this.on = function(settings) {
-    this.synth.triggerAttack(settings[LayerType.PITCH].value);
+    const pitch = settings[LayerType.PITCH]?.value;
+    if (pitch !== undefined) this.synth.triggerAttack(pitch);
   };
   this.off = () => this.synth.triggerRelease();
 }
